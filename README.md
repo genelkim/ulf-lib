24543949e99ad5e5c206970965d79f2a8fe816fd
Gene Kim <gkim21@cs.rochester.edu>
2019-01-24 15:32:27 -0500

    Added load.lisp for installation testing purposes.  Added installation instructions to README.md

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # ulf-lib
 Library for interfacing with and manipulating unscoped episodic logical forms (ULF).  
+
+## Dependencies
+- Allegro Common Lisp Regex Library (comes with allegro common lisp distribution)
+- Quicklisp
+- TTT (get a copy at http://cs.rochester.edu/u/gkim21/data/ttt.tar.gz)
+- https://github.com/genelkim/cl-util
+- cl-strings (loaded automatically via quicklisp)
+
+## Installation
+1. Install quicklisp by following instructions at https://www.quicklisp.org/beta/
+2. Then place the other depenedencies listed above in a folder accessible to Quicklisp or ASDF (which underlies quicklisp).  How to do this in a couple ways is described by the following Stack Overflow answer https://stackoverflow.com/a/11265601.
+
+## Running the Code
+This is really meant to be a library, but to check the basic functionality of any of the functions, you can load the file load.lisp and enter the package :ulf-lib.  For example,
+```
+$ acl
+$ (load "load.lisp")
+$ ...[loading messages]...
+$ (in-package :ulf-lib)
+$ (ulf-type? 'man.n)
+(NOUN PRED)
+$ (ulf-type? '|John|)
+(TERM)
+$ (ulf-type? '(the.d (angry.a man.n)))
+(TERM)
+$ (ulf-type? '(see.v the.d man.n))
+(UNKNOWN)
+$ (apply-sub-macro '((sub what.pro ((past do.aux-s) he.pro (say.v *h))) ?))
+(((PAST DO.AUX-S) HE.PRO (SAY.V WHAT.PRO)) ?)
+```
---
+++ load.lisp
@@ -0,0 +1,44 @@
+;; ASDF system definition for ulf-lib
+;; Original version of this file written for Epilog.
+(require 'asdf)
+
+;; avoids saving compiled files in special local cache.
+(let f (and (setq f (fboundp (find-symbol "DISABLE-OUTPUT-TRANSLATIONS" 'asdf)))
+	    (funcall f)))
+
+;; from http://www.cliki.net/asdf
+;;; If the fasl was stale, try to recompile and load (once). Since only SBCL
+;;; has a separate condition for bogus fasls we retry on any old error
+;;; on other lisps. Actually, Allegro has a similar condition, but it's
+;;; unexported.  Works nicely for the ACL7 upgrade, though.
+;;; CMUCL has an invalid-fasl condition as of 19c.
+(defmethod asdf:perform :around ((o asdf:load-op) (c asdf:cl-source-file))
+  (handler-case (call-next-method o c)
+    (#+sbcl sb-ext:invalid-fasl
+     #+allegro excl::file-incompatible-fasl-error
+     #+lispworks conditions:fasl-error
+     #+cmu ext:invalid-fasl
+     #-(or sbcl allegro lispworks cmu) error ()
+     (asdf:perform (make-instance 'asdf:compile-op) c)
+     (call-next-method))))
+
+;(asdf:initialize-source-registry
+;  `(:source-registry
+;      :ignore-inherited-configuration
+;      (:tree ,(namestring (merge-pathnames "./")))))
+
+;; Adds the directory containing load.lisp to the "PATH" of asdf, so
+;; we can load *util* from any directory.
+(push (make-pathname :directory (pathname-directory *load-truename*))
+      asdf:*central-registry*)
+
+
+;; compiler settings
+(proclaim '(optimize (speed 3) (safety 3) (space 0) (debug 3)))
+
+;; Load Util Choose between the following two lines depending on
+;; whether you want the files compiled into FASLs or not:
+(asdf:operate 'asdf:load-op 'ulf-lib) ;; Compile and load as necessary
+;(asdf:operate 'asdf:load-source-op 'ttt) ;; Doesn't compile
+
+
