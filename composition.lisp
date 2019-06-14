06ad49ce20951a0787be83069ac57d5600aff640
Mandar Juvekar <mjuvekar7@gmail.com>
2019-06-14 14:22:58 -0400

    Add support for lambda expressions.

--- composition.lisp
+++ composition.lisp
@@ -3,7 +3,9 @@
 
 (in-package :ulf-lib)
 
-;; TODO: Deal with lambdas
+; Big limitation: Cases where two types can be matched by assigning values to
+; exponent variables cannot be handled. An example is combining (2=>2) with
+; (D^(- N 1)=>2), where N needs to be set to 1.
 
 ;; Decrement a given exponent field. If e is a number, simply decrement. If not,
 ;; return the list '(- e 1)
@@ -42,6 +44,9 @@
               (setf (ex (domain temp)) (decr-exp (ex (domain temp))))
               temp)))
         ; Not an exact match, but could still be possible using variable assignments
+        ; Possible idea to handle cases where leftmost atom can be made to
+        ; disappear with a 0 exponent: if that is possible, wrap it in an
+        ; optional without the leftmost atom
         NIL)))) ; TODO
 
 ;; Compose two types if possible and return the composed type. Also return the
@@ -70,11 +75,24 @@
 ;; Given a ULF, evaluate and return the type if possible. Currently assumes
 ;; left associativity if there are more than 2 items scoped together. This is
 ;; not ideal, and needs to be changed.
-(defun ulf-type? (ulf)
+;; Note: Unknown ulf atoms are ignored.
+(defun ulf-type? (ulf &key lambda-vars)
   (if (symbolp ulf)
-    (atom-semtype? ulf)
-    (if (= (length ulf) 1)
-      (ulf-type? (car ulf))
-      (compose-types! (ulf-type? (reverse (cdr (reverse ulf)))) (ulf-type? (car (last ulf)))))))
-;      (compose-types! (ulf-type? (car ulf)) (ulf-type? (cdr ulf))))))
+    ; atomic ULF
+    (if (atom-semtype? ulf)
+      (atom-semtype? ulf)
+      (when (member ulf lambda-vars) (str2semtype "D")))
+
+    ; non-atomic ULF
+    (if (equal (car ulf) 'lambda)
+      ; ULF is of the form (lambda var (expr))
+      (when (= (length ulf) 3)
+        (make-instance 'semtype
+                       :domain (str2semtype "D")
+                       :range (ulf-type? (third ulf) :lambda-vars (cons (cadr ulf) lambda-vars))))
+      ; ULF is neither a lambda nor an atom
+      (if (= (length ulf) 1)
+        (ulf-type? (car ulf) :lambda-vars lambda-vars)
+        (compose-types! (ulf-type? (reverse (cdr (reverse ulf))) :lambda-vars lambda-vars)
+                        (ulf-type? (car (last ulf)) :lambda-vars lambda-vars))))))
 
